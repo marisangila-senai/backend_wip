01abf1b2eb3ab0a55b864394a80d6ee7f320c0fe
marisangila-senai <marisangila.alves@edu.sc.senai.br>
2025-09-19 14:07:49 -0300

    Add files via upload

---
+++ backend_wip/source/listar.php
@@ -0,0 +1,18 @@
+<?php
+    include_once("conexao.php");
+
+    $sql = $conn->prepare("SELECT pk_usuario, email_usuario,
+    eh_adm_usuario FROM usuario;");
+
+    $sql->execute();
+
+    if ($sql->rowCount() >= 1) {
+        $usuarios = $sql->fetchAll();
+    } else {
+        echo '<div class="alert alert-warning fixed-bottom" role="alert">
+            Não há usuários cadastrados!
+        </div>';
+    }
+    unset($sql);
+    unset($conn);
+?>
---
+++ backend_wip/source/login.php
@@ -0,0 +1,27 @@
+<?php
+include_once("conexao.php");
+
+$email = $_POST["email"];
+$senha = MD5($_POST["senha"]);
+
+$sql = $conn->prepare("SELECT pk_usuario, email_usuario, 
+    senha_usuario, eh_adm_usuario FROM usuario WHERE email_usuario = ?");
+
+$sql->execute([$email]);
+
+if ($sql->rowCount() == 1) {
+    $usuario = $sql->fetch();
+    if ($usuario['senha_usuario'] == $senha) {
+        session_start();
+        $_SESSION["eh_adm_usuario"] = $usuario['eh_adm_usuario'];
+        $_SESSION['loggedin'] = true;
+
+        header("location:../pages/pagina_inicial.php");
+    } else {
+        echo ("E-mail ou senha inválidos!");
+    }
+} else {
+    echo ("E-mail ou senha inválidos!");
+}
+unset($sql);
+unset($conn);
